0bded44fe1e8645e94cbc0ed79cf4b846fac00aa
David Catalim <davidcatalim@gmail.com>
2025-09-01 23:58:36 +0100

    replaced favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import { ModeWatcher } from 'mode-watcher';
 	import { Toaster } from '$lib/components/ui/sonner/index.js';
 
@@ -8,7 +7,7 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href='/favicon.png' />
 </svelte:head>
 
 <Toaster />
